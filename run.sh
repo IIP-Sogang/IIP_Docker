bded35f26d0e6acfcc500de872156267944d35e0
kooBH <k01032633280@gmail.com>
2019-07-05 17:17:27 +0900

    volume

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #! /bin/sh
 #docker build . -t alt:v1
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH iip:v4
+
+
+### For matlab GUI installation  ###
+# XSOCK=/tmp/.X11-unix
+# XAUTH=/tmp/.docker.xauth
+# xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+# docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH iip:v4
+
+
+# 그냥 . 찍는 거는 인식 안됨 $PWD 를 쓰거나
+# 절대경로 입력
+ docker run -it --name iip -v $PWD/data:/home/iip/data/ iip:v4
+
+ ./all_in_one/remove_exited.sh
