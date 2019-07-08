628d2003ac4884ca3a55cce86a28b856de158c4f
kbh <nine4409@u.sogang.ac.kr>
2019-07-08 16:01:30 +0900

    comments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM iip:v6
+# iip:v7 이미지를 기반으로 생성합니다.
+FROM iip:v7
 
-#RUN apt-get update
-#RUN apt-get install -qqy x11-apps
-#ENV DISPLAY :0
+#### X11 forwarding 용 구문 ####
+# RUN apt-get update
+# RUN apt-get install -qqy x11-apps
+# ENV DISPLAY :0
 
+# 현재 폴더의 mnt 폴더를 docker container의 /mnt 폴더에 마운트합니다.
+# 마운트는 파일을 공유하지 않습니다.
+# ./mnt 의 내용을 /mnt에 올리는 행위입니다.
+# host 와 container 간 파일 공유는 run.sh 에서 이루어집니다.
 ADD ./mnt /mnt
-
-CMD /bin/bash
--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #! /bin/sh
-#XSOCK=/tmp/.X11-unix
-#XAUTH=/tmp/.docker.xauth
-#xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-#docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH iip:v4
+
+### X11 forwarding 용 구문 ###
+# XSOCK=/tmp/.X11-unix
+# XAUTH=/tmp/.docker.xauth
+# xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+# docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH iip:v4
 
 
-docker build . -t iip:v6
-docker run --name working -v ${PWD}/data/:/home/data/ -it iip:v6 /bin/sh
+# 현재 폴더에서 Dockerfile 로 iip:v7 라는 이미지를 생성합니다. 
+docker build . -t iip:v7
+# iip:v6 이미지로 working 이라는 이름을 가진 컨테이너를 만듭니다.
+# 현재 폴더의 data 폴더를 docker conatiner의 /home/data 폴더와 공유합니다.
+# host와 docker container 양쪽에서 같은 파일을 조작할 수 있습니다.
+# 터미널로 /bin/sh를 실행합니다.
+docker run --name working -v ${PWD}/data/:/home/data/ -it iip:v7 /bin/sh
 
