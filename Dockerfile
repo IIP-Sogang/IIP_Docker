8e1be274f3b176dcda2f68c945ca0581a4d3da1c
kooBH <k01032633280@gmail.com>
2019-07-08 14:58:48 +0900

    7.7v1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-FROM iip/demo:v1
-RUN apt-get update
-RUN apt-get install -qqy x11-apps
-ENV DISPLAY :0
+FROM iip:v6
+
+#RUN apt-get update
+#RUN apt-get install -qqy x11-apps
+#ENV DISPLAY :0
+
+ADD ./mnt /mnt
+
 CMD /bin/bash
--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
-#docker build . -t alt:v1
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH iip:v4
+#XSOCK=/tmp/.X11-unix
+#XAUTH=/tmp/.docker.xauth
+#xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+#docker run -ti -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH iip:v4
+
+
+docker build . -t iip:v6
+docker run --name working -v ${PWD}/data/:/home/data/ -it iip:v6 /bin/sh
